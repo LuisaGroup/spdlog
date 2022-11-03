e46d8278a7f41f82a5ecba8bd35517198e363fcd
maxwellgeng@outlook.com <maxwellgeng@outlook.com>
2022-11-03 18:06:52 +0800

    Create xmake.lua

---
+++ xmake.lua
@@ -0,0 +1,15 @@
+BuildProject({
+	projectName = "spdlog",
+	projectType = "shared",
+	unityBuildBatch = 64
+})
+add_includedirs("../fmt/include/", "include/", {
+	public = true
+})
+add_defines("SPDLOG_NO_EXCEPTIONS", "SPDLOG_NO_THREAD_ID", "SPDLOG_DISABLE_DEFAULT_LOGGER", "SPDLOG_COMPILED_LIB",
+				"FMT_SHARED", "SPDLOG_FMT_EXTERNAL", "SPDLOG_SHARED_LIB", "FMT_HEADER_ONLY=1", "FMT_CONSTEVAL=constexpr",
+				"FMT_USE_CONSTEXPR=1", "FMT_EXCEPTIONS=0", {
+					public = true
+				})
+add_defines("FMT_EXPORT", "spdlog_EXPORTS")
+add_files("src/*.cpp|src/fmt.cpp")

1c47b32a9afaae82f6d17057a72eb38fd163cf33
maxwellgeng@outlook.com <maxwellgeng@outlook.com>
2022-11-06 21:46:13 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -3,10 +3,10 @@ BuildProject({
 	projectType = "shared",
 	unityBuildBatch = 64
 })
-add_includedirs("../fmt/include/", "include/", {
+lc_add_includedirs("../fmt/include/", "include/", {
 	public = true
 })
-add_defines("SPDLOG_NO_EXCEPTIONS", "SPDLOG_NO_THREAD_ID", "SPDLOG_DISABLE_DEFAULT_LOGGER", "SPDLOG_COMPILED_LIB",
+lc_add_defines("SPDLOG_NO_EXCEPTIONS", "SPDLOG_NO_THREAD_ID", "SPDLOG_DISABLE_DEFAULT_LOGGER", "SPDLOG_COMPILED_LIB",
 				"FMT_SHARED", "SPDLOG_FMT_EXTERNAL", "SPDLOG_SHARED_LIB", "FMT_HEADER_ONLY=1", "FMT_CONSTEVAL=constexpr",
 				"FMT_USE_CONSTEXPR=1", "FMT_EXCEPTIONS=0", {
 					public = true

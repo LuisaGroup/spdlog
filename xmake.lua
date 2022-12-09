b0871812497bbc3a488016e7a36566e47d3c4afd
maxwellgeng@outlook.com <maxwellgeng@outlook.com>
2022-12-09 10:50:14 +0800

    use internal fmt

--- xmake.lua
+++ xmake.lua
@@ -7,13 +7,13 @@ _config_project({
 })
 local add_includedirs = _get_add_includedirs()
 local add_defines = _get_add_defines()
-add_includedirs("../fmt/include/", "include/", {
+add_includedirs("include/", {
 	public = true
 })
 add_defines("SPDLOG_NO_EXCEPTIONS", "SPDLOG_NO_THREAD_ID", "SPDLOG_DISABLE_DEFAULT_LOGGER", "SPDLOG_COMPILED_LIB",
-				"FMT_SHARED", "SPDLOG_FMT_EXTERNAL", "SPDLOG_SHARED_LIB", "FMT_HEADER_ONLY=1", "FMT_CONSTEVAL=constexpr",
+				"FMT_SHARED", "SPDLOG_SHARED_LIB", "FMT_CONSTEVAL=constexpr",
 				"FMT_USE_CONSTEXPR=1", "FMT_EXCEPTIONS=0", {
 					public = true
 				})
 add_defines("FMT_EXPORT", "spdlog_EXPORTS")
-add_files("src/*.cpp|src/fmt.cpp")
+add_files("src/*.cpp")

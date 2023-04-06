2e15ac77801a210f60eb3fba83c2fcbdc359b48f
Maxwell Geng <maxwellgeng@outlook.com>
2023-04-06 16:09:41 +0800

    minor

--- xmake.lua
+++ xmake.lua
@@ -9,9 +9,9 @@ add_includedirs("include/", {
 	public = true
 })
 add_defines("SPDLOG_NO_EXCEPTIONS", "SPDLOG_NO_THREAD_ID", "SPDLOG_DISABLE_DEFAULT_LOGGER", "SPDLOG_COMPILED_LIB",
-				"FMT_SHARED", "SPDLOG_SHARED_LIB", "FMT_CONSTEVAL=constexpr",
-				"FMT_USE_CONSTEXPR=1", "FMT_EXCEPTIONS=0", {
+				"FMT_SHARED", "SPDLOG_SHARED_LIB", "FMT_CONSTEVAL=constexpr", "FMT_USE_CONSTEXPR=1", "FMT_EXCEPTIONS=0", {
 					public = true
 				})
 add_defines("FMT_EXPORT", "spdlog_EXPORTS")
 add_files("src/*.cpp")
+target_end()
